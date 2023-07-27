568bb376414c5510ebeecc457a86f36012fc7e82
FacuMozo <facumozo98@gmail.com>
2023-07-27 15:47:12 -0300

    se agrega icono de exit fullscreen y logica para entrar y salir de fullscreen

--- src/app/core-module/navbar/navbar.component.ts
+++ src/app/core-module/navbar/navbar.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+    icon = "fullscreen";
+
+    fullscreen: boolean = false;
+    
+    toggleFullscreen(){
+      if(document.fullscreenEnabled){
+        let elem = document.documentElement;
+        this.fullscreen = !this.fullscreen;
+        
+        if(this.fullscreen){
+          elem.requestFullscreen();
+          this.icon ="fullscreen-exit"
+        }else{
+          this.icon ="fullscreen"
+          document.exitFullscreen();
+        }
+      }
+    }
 }
